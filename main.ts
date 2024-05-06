790d5d8558e2c371db92ab7e445572bdcc8ab499
Muhammad Maaz <166921901+MuhammadMuaazMk@users.noreply.github.com>
2024-05-06 22:27:58 +0500

    Simple Number Guessing B/w 1 to 10

---
+++ main.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer"
+const randomNumber = Math.floor(Math.random()*10+1);
+const answer= await inquirer.prompt([
+    { 
+        name:"userGuessedNumber",
+        type: "number",
+        message:"Please guess a Number between 1 to 10 :"
+
+    }
+
+]);
+
+if(answer.userGuessedNumber === randomNumber){
+    console.log("Congurlation!! You are Guess Number is Right")
+}
+else{
+    console.log("Your guess Number is Not Right")
+}
